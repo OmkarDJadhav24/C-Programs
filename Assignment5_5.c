df4403737c424739647be3550b04d40cc0c69c59
OmkarDJadhav24 <101233431+OmkarDJadhav24@users.noreply.github.com>
2022-03-29 21:48:05 +0530

    Add files via upload

---
+++ Assignment5_5.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int Check(int No)
+{
+	int i=0;
+	int icnt=0;
+	int N=0;
+	while(No>0)
+	{
+		N=No%10;
+		if(N<6)
+		{
+			icnt=icnt+1;
+		}
+		No=No/10;
+	}
+	return icnt;
+}
+
+int main()
+{
+	int No=0;
+	int Result=0;
+	
+	printf("\nEnter the number:");
+	scanf("%d",&No);
+	
+	Result=Check(No);
+	printf("Frequency of numbers less than 6: %d",Result);
+	return 0;
+}
